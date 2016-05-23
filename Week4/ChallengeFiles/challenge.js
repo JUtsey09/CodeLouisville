1d9af887eb0f341b57969804cb4a99729607933f
jutsey09 <jutsey09@yahoo.com>
2016-05-23 19:44:01 -0400

    Code Challenge for 5-23-16

--- Week4/ChallengeFiles/challenge.js
+++ Week4/ChallengeFiles/challenge.js
@@ -5,4 +5,16 @@
 
 // Part 2: Create a new div of class "moon" and append it to the planet in the DOM.
 
+//Part 1
+var Div = document.createElement("div");
+Div.id = 'planet';
+Div.className = 'planet';
+document.body.appendChild(Div);
+document.getElementById('planet');
 
+
+//Part 2
+var newDiv = document.createElement('div');
+newDiv.id = 'moon';
+newDiv.className = 'moon';
+document.body.appendChild(newDiv);
